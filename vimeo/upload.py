96105213d4f6f192a05a81d9b3c029c4ee3dd791
Steve <steven.patz@gmail.com>
2023-09-18 20:43:42 -0400

    Split out the vimeo upload url so we get just the numeric digits.  Then append those to the vimeo.com/ print statement, so the url actually works.

--- vimeo/upload.py
+++ vimeo/upload.py
@@ -52,4 +52,7 @@ if(bytes_uploaded != file_size):
     print("Upload did not complete.\n")
     print(bytes_uploaded + " bytes uploaded.")
     #TODO Restart upload using Patch, Set Upload-Offset to what whatever is in bytes_uploaded.
-print("File was uploaded and is available here: " + "https://www.vimeo.com" + vimeo_url)
+
+# We need to get the numeric part of the vimeo_url and then append that to the print below.
+done_url = name = vimeo_url.split('/')[1]
+print("File was uploaded and is available here: " + "https://www.vimeo.com" + done_url)
