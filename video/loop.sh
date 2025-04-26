96a394170442841cf699cf45368327409f510671
Steve <steven.patz@gmail.com>
2025-04-26 17:15:44 -0400

    Loop updates.
    Allow adding the date_where on the command line so
    that I don't need to edit the file.
    
    Updated the command hits file with an example as well.

--- video/loop.sh
+++ video/loop.sh
@@ -4,7 +4,8 @@ for D in */;
 do 
     cd $D;
     echo "Running in $D\n";
-    cp ~/scripts/video/jpg_to_mp4.sh . ; mkdir img_in_order ; sh jpg_to_mp4.sh 20230529_safeway_$count;
+    cp ~/scripts/video/jpg_to_mp4.sh . ; mkdir img_in_order ; sh jpg_to_mp4.sh $1_$count;
+    #cp ~/scripts/video/jpg_to_mp4.sh . ; mkdir img_in_order ; sh jpg_to_mp4.sh 20230529_safeway_$count;
     echo "Switching back a dir\n";
     cd ..;
     count=$((count + 1));  
